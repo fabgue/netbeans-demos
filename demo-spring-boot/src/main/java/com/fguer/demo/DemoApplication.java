23b89413d48370887b9704f7fc1b0ace35da325e
fabgue <fabgue@ymail.com>
2017-12-31 19:00:01 -0500

    Fixing demo

--- demo-spring-boot/src/main/java/com/fguer/demo/DemoApplication.java
+++ demo-spring-boot/src/main/java/com/fguer/demo/DemoApplication.java
@@ -4,11 +4,11 @@ import java.util.Collection;
 import java.util.stream.Stream;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -16,60 +16,58 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class DemoSpringBootApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringBootApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@Entity
+class Reservation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String reservationName;
+
+    public Long getId() {
+        return id;
     }
 
-    @Entity
-    class Reservation {
-
-        @Id
-        @GeneratedValue
-        private Long id;
-        private String reservationName;
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getReservationName() {
-            return reservationName;
-        }
-
-        public Reservation(String reservationName) {
-            this.reservationName = reservationName;
-        }
-
-        public Reservation() {
-        }
-
-        @Override
-        public String toString() {
-            return "Reservation{" + "id=" + id + ", reservationName=" + reservationName + '}';
-        }
+    public String getReservationName() {
+        return reservationName;
     }
 
-    
-    @RepositoryRestResource
-    interface ReservationRepository extends JpaRepository {
-
-        @RestResource(path = "by-name")
-        Collection findByReservationName(@Param("rn") String rn);
+    public Reservation(String reservationName) {
+        this.reservationName = reservationName;
     }
 
-    
-    @Component
-    class DummyCLR implements CommandLineRunner {
+    public Reservation() {
+    }
 
-        @Autowired
-        private ReservationRepository repository;
+    @Override
+    public String toString() {
+        return "Reservation{" + "id=" + id + ", reservationName=" + reservationName + '}';
+    }
+}
 
-        @Override
-        public void run(String... args) throws Exception {
-            Stream.of("Josh", "Geertjan", "Pieter").forEach(x -> repository.save(new Reservation(x)));
-            repository.findAll().forEach(System.out::println);
-        }
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @RestResource(path = "by-name")
+    Collection findByReservationName(@Param("rn") String rn);
+}
+
+@Component
+class DummyCLR implements CommandLineRunner {
+
+    @Autowired
+    private ReservationRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Stream.of("Josh", "Geertjan", "Pieter").forEach(x -> repository.save(new Reservation(x)));
+        repository.findAll().forEach(System.out::println);
     }
 }
