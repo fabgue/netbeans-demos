6c2818eb47f87e7faf24f58964394dee3ea5c1e2
Fabio Guerrero <fabgue@ymail.com>
2019-01-23 00:42:29 -0500

    OAuth implicit okta

--- oauth-implicit/src/main/java/com/fguer/oauth/MessageController.java
+++ oauth-implicit/src/main/java/com/fguer/oauth/MessageController.java
@@ -1,6 +1,7 @@
 package com.fguer.oauth;
 
 import java.security.Principal;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     
     @GetMapping("/message")
+    @PreAuthorize("hasAuthority('Everyone') || #oauth2.hasScope('email')")
     public String getMessage(Principal principal) {
         return "The message is for user: " + principal.getName();
     }
--- oauth-implicit/src/main/java/com/fguer/oauth/OAuthImplicitApplication.java
+++ oauth-implicit/src/main/java/com/fguer/oauth/OAuthImplicitApplication.java
@@ -3,9 +3,13 @@ package com.fguer.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @EnableResourceServer
 @SpringBootApplication
@@ -21,10 +25,17 @@ public class OAuthImplicitApplication {
             @Override
             public void configure(HttpSecurity http) throws Exception {
                 http.authorizeRequests()
-                    .antMatchers("/", "/index.html", "/sign-in-widget-config").permitAll()
-                    .anyRequest().authenticated();
+                        .antMatchers("/", "/index.html", "/sign-in-widget-config").permitAll()
+                        .anyRequest().authenticated();
             }
         };
     }
 
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    protected static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            return new OAuth2MethodSecurityExpressionHandler();
+        }
+    }
 }
