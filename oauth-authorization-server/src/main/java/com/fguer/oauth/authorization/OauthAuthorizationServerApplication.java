f565f4e3177b7f6228f19921739f98ee35bc0c29
Fabio Guerrero <fabgue@ymail.com>
2019-02-14 23:27:43 -0500

    oauth-server

---
+++ oauth-authorization-server/src/main/java/com/fguer/oauth/authorization/OauthAuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package com.fguer.oauth.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthorizationServerApplication.class, args);
+	}
+
+}
+
